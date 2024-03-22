8b1e2a6e1ab431990930954db5030afa4e59d528
vmaksimov <viktor.maksimov@oblak.bg>
2024-03-22 15:48:45 +0200

    optimizating html elements

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,6 +6,7 @@ const SearchBox = ({searchField}) => {
     return (
         <div className="pa2">
             <input
+                aria-label="Search Robots"
                 className="pa3 ba b--green bg-lightest-blue"
                 type="search"
                 placeholder="search robots"
