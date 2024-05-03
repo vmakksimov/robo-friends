b18d83cdbe94b2b343d9e6b7f5b1554695f7577c
vmaksimov <viktor.maksimov@oblak.bg>
2024-05-03 18:20:13 +0300

    adding yaml for ci

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -3,16 +3,16 @@ import CounterButton from "./CounterButton";
 import React from "react";
 
 
-it('counter button test', () => {
-    const mockColor = 'red'
-    expect(shallow(<CounterButton color={mockColor}/>)).toMatchSnapshot();
-})
-
-// it('correctly icrements counter', () => {
+// it('counter button test', () => {
 //     const mockColor = 'red'
-//     const wrapper = (shallow(<CounterButton color={mockColor}/>))
-//     wrapper.find('[id="counter"]').simulate('click')
-//     wrapper.find('[id="counter"]').simulate('click')
-//     expect(wrapper.state()).toEqual({count: 2})
-//     expect(wrapper.props().color).toEqual('red')
+//     expect(shallow(<CounterButton color={mockColor}/>)).toMatchSnapshot();
 // })
+
+it('correctly icrements counter', () => {
+    const mockColor = 'red'
+    const wrapper = (shallow(<CounterButton color={mockColor}/>))
+    wrapper.find('[id="counter"]').simulate('click')
+    wrapper.find('[id="counter"]').simulate('click')
+    expect(wrapper.state()).toEqual({count: 2})
+    expect(wrapper.props().color).toEqual('red')
+})
